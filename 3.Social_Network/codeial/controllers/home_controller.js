a57abdb53b1741280ad1ef252ca9a4360996c081
Sahil Mahale <sahilmahale0@gmail.com>
2021-01-22 20:53:39 +0530

    displaying comments on screen/page

--- 3.Social_Network/codeial/controllers/home_controller.js
+++ 3.Social_Network/codeial/controllers/home_controller.js
@@ -2,12 +2,19 @@ const Post = require('../models/post');
 
 module.exports.home = function(req, res){
     //populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    //populate also called preloading (preloading  multiple objects here)
+    Post.find({}).populate('user')  
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title:'Codeial | home',
             posts:posts
         });
-
     });
 }
 
