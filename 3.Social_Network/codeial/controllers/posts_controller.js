163b72e56dc514c2c5be6d9dd4cae62e638e78c9
Sahil Mahale <sahilmahale0@gmail.com>
2021-01-25 14:10:08 +0530

    Controller done for DeletingPost(authorization)

--- 3.Social_Network/codeial/controllers/posts_controller.js
+++ 3.Social_Network/codeial/controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
 
 
 module.exports.create=function(req,res){
@@ -11,6 +12,42 @@ module.exports.create=function(req,res){
         }
         return res.redirect('back')
     });
-
-
 }
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means concerting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post:req.param.id},function{
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
