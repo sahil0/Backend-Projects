bfb5413d0ce2dfaa9fc9364e99d71c1232b6db49
Sahil Mahale <sahilmahale0@gmail.com>
2021-02-26 20:31:41 +0530

    commit message

--- 3.Social_Network/codeial/controllers/home_controller.js
+++ 3.Social_Network/codeial/controllers/home_controller.js
@@ -2,25 +2,31 @@ const Post = require('../models/post');
 const User = require('../models/user');
 const Use = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home =async function(req, res){
     //populate the user of each post
     //populate also called preloading (preloading  multiple objects here)
-    Post.find({}).populate('user')  
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title:'Codeial | home',
-                posts:posts,
-                all_users:users,
-             });//Now all the users available to us we just need to show them in home
+    try{
+        //populate the user of each post
+        let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
         });
-    });
+    let users= await User.find({});
+    return res.render('home',{
+        title:'Codeial | home',
+        posts:posts,
+        all_users:users,
+     });
+    }catch(err){
+        console.log('Error',err);
+        return;
+
+    }
 }
+
 
 // module.exports.actionName = function(req, res){}
