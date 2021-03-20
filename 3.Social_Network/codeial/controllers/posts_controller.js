6f1db6bb7a4fae03e63feae53880f79c26456ae9
Sahil Mahale <sahilmahale0@gmail.com>
2021-03-20 11:10:28 +0530

    changed the controllers code to Asynch

--- 3.Social_Network/codeial/controllers/posts_controller.js
+++ 3.Social_Network/codeial/controllers/posts_controller.js
@@ -2,30 +2,38 @@ const Post = require('../models/post')
 const Comment = require('../models/comment')
 
 
-module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    },function(err,post){
-        if(err){
-            console.log('error in creating a post');
-        }
-        return res.redirect('back')
+module.exports.create= async function(req,res){
+    try{
+        await Post.create({
+            content:req.body.content,
+            user:req.user._id
     });
+    return res.redirect('back')
+    }
+    catch(err){
+        console.log('Error',err)
+        return;
+        
+    }
 }
-module.exports.destroy=function(req,res){
-    Post.findById(req.params.id,function(err,post){
-        //.id means concerting the object id into string
+module.exports.destroy=async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+            //.id means concerting the object id into string
         if(post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post:req.param.id},function(err){
-                return res.redirect('back');
-            });
-        }else{
+            await Comment.deleteMany({post:req.param.id});
+            return res.redirect('back');
+        }else
+        {
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
+
 
 
 
